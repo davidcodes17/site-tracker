542cbc80473beeaef0b9e1d137edfef9f7045eae
davidcodes17 <areegbedavid@gmail.com>
2024-04-29 16:30:17 +0100

    fix get-location-comment-token part

--- src/app/api/admin/get-location/route.ts
+++ src/app/api/admin/get-location/route.ts
@@ -5,25 +5,25 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   try {
-    const token = req.headers.get("authorization")
-    console.log(token)
-    if (!token) {
-      return NextResponse.json({ msg: "Invalid Token" }, { status: 400 });
-    }
+    // const token = req.headers.get("authorization")
+    // console.log(token)
+    // if (!token) {
+    //   return NextResponse.json({ msg: "Invalid Token" }, { status: 400 });
+    // }
 
-    const [_ ,tk] = token.split(" ")
+    // const [_ ,tk] = token.split(" ")
 
-    const decode = jwt.verify(tk, process.env.JWT_SECRET!);
-    const u = await db.user.findUnique({
-      where: {
-        //@ts-expect-error
-        uuid: decode.id,
-        role: 0,
-      },
-    });
-    if (!u) {
-      return NextResponse.json({ msg: "Invalid Token" }, { status: 404 });
-    }
+    // const decode = jwt.verify(tk, process.env.JWT_SECRET!);
+    // const u = await db.user.findUnique({
+    //   where: {
+    //     //@ts-expect-error
+    //     uuid: decode.id,
+    //     role: 0,
+    //   },
+    // });
+    // if (!u) {
+    //   return NextResponse.json({ msg: "Invalid Token" }, { status: 404 });
+    // }
 
     const locations = await db.locations.findMany({});
     return NextResponse.json(
