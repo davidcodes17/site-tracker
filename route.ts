09a9330ee84e4eb40f084ea63cb7495b65fd68c9
davidcodes <116895551+davidcodes17@users.noreply.github.com>
2024-04-29 02:06:19 +0100

    Update route.ts

--- route.ts
+++ route.ts
@@ -1,2 +1,1 @@
-// export const HOST = "https://ecommerce-sales-shop.vercel.app/api/admin";
-export const HOST = "http://localhost:3000/api/admin";
+export const HOST = "https://ecommerce-sales-shop.vercel.app/api/admin";
